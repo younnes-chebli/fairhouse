d2024bb89ce14766f69846a1c20d1d59e17dfa65
Younnes Chebli <younnesc@gmail.com>
2023-06-22 13:41:12 +0200

    feat: added event handling and search results

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-list',
@@ -12,4 +13,10 @@ export class HousingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  searchHousingLocations(searchText: string) {
+    console.log(searchText);
+    
+  }
+
+  @Input() locationList: HousingLocation[] = [];
 }
