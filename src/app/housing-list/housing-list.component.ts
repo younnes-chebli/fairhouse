479b051892fc83635377affc6d880f51e67d167a
Younnes Chebli <younnesc@gmail.com>
2023-07-14 01:05:47 +0200

    refactor

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -7,6 +7,8 @@ import { HousingLocation } from '../housing-location';
   styleUrls: ['./housing-list.component.css']
 })
 export class HousingListComponent implements OnInit {
+  @Input() locationList: HousingLocation[] = [];
+  results: HousingLocation[] = [];
 
   constructor() { }
 
@@ -14,8 +16,7 @@ export class HousingListComponent implements OnInit {
   }
 
   searchHousingLocations(searchText: string) {
-    if (!searchText)
-      return;
+    if (!searchText) return;
 
     this.results = this.locationList.filter(
       (location: HousingLocation) => location.city
@@ -24,7 +25,4 @@ export class HousingListComponent implements OnInit {
           searchText.toLowerCase()
     ));
   }
-
-  @Input() locationList: HousingLocation[] = [];
-  results: HousingLocation[] = [];
 }
